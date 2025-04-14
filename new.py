30fafdc186fdca55a18701126298a46bbd6612b7
Mohammed Moshood <mohammedamoshood@gmail.com>
2025-04-14 16:26:45 +0100

    jupyter config issue fixed. first graph finally appearing. lfgggggggggg

---
+++ new.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3], [4, 2, 5])
+plt.title("Now from Jupyter")
+plt.show()
---
+++ test.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3], [3, 2, 1])
+plt.title("Post-surgery Plot")
+plt.show()
